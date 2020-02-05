310ae48e345702cc81a6bab2261415357ec67094
SpaceDucK <marko@kth.se>
2020-02-05 14:32:01 +0100

    VMD trajectory short

--- Prototypes/P1_trial_run/trial.py
+++ Prototypes/P1_trial_run/trial.py
@@ -1,6 +1,9 @@
 import gmxapi as gmx
 # import numpy as np
 
+## create tpr file:
+## gmx grompp -f md.mdp -c conf.gro -p topol.top -o topol.tpr -maxwarn 3
+
 ## numpy  and networkx needed for initial testrun
 
 trial_input = gmx.read_tpr("topol.tpr")
