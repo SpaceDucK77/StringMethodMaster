bb0db24665317efc7ed0a8cfd3f293fea44980a8
oliver <oliver.fleetwood@scila.se>
2020-05-20 17:10:41 +0200

    Added sumbission script

---
+++ Prototypes/Oliver_P9/submit_beskow.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Include your allocation number
+#SBATCH -A 2019-2-34
+
+# The name of the job in the queue
+#SBATCH -J markoStringMethod
+
+# Total number of nodes
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node=32
+
+# length in hours
+#SBATCH -t 00:59:59
+
+# Receive e-mails when your job starts and ends
+#SBATCH --mail-user=oliver.fleetwood@scilifelab.se --mail-type=FAIL
+
+# Output file names for stdout and stderr
+#SBATCH -e error.log
+#SBATCH -o output.log
+
+module swap PrgEnv-cray PrgEnv-gnu
+module load gromacs/2020.1
+export OMP_NUM_THREADS=1
+export GMX_MAXBACKUP=-1
+echo "LOADED MODULES"
+
+source activate
+cmd="srun -n 1 python VIS_col.py"
+echo $cmd
+$cmd
