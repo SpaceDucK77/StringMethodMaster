f2257909d8cbe33fec1960c337c342b3d546f009
SpaceDucK <marko@kth.se>
2020-06-01 10:52:30 +0200

    changes that seem to not have sticked

--- Prototypes/Oliver_P9/md_tools.py
+++ Prototypes/Oliver_P9/md_tools.py
@@ -53,6 +53,8 @@ def backup_file(file_name, copy):
     try:
         open(file_name)
     except FileNotFoundError:
+        f = open(file_name,"w")
+        f.close()
         return file_name
     while not done:
         bu_name = f1 + "_backup_" + str(backup_no) + "_." + f2
